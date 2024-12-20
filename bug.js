6a751d0543e34b3e0f9d615c5154a5aa63d93120
BugHunter-X <askzenai@gmail.com>
2024-12-20 23:30:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,23 @@
+```javascript
+//pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <Link href='/about'>
+        <a>Go to About Page</a>
+      </Link>    </div>
+  );
+}
+```
+```javascript
+//pages/about.js
+function About(){
+    return(
+        <h1>About Page</h1>
+    );
+}
+export default About;
+```
